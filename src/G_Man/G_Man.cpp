ae8fe7fb9352db5ae05ce3e45e38cea9f90d0f7e
RVeips <rihardsveips@gmail.com>
2022-01-30 20:52:47 +0200

    ref

--- src/G_Man/G_Man.cpp
+++ src/G_Man/G_Man.cpp
@@ -1,24 +1,25 @@
 // ---------------------------------------------------------------------
 // CFXS Framework CNC Module <https://github.com/CFXS/CFXS-CNC>
 // Copyright (C) 2022 | CFXS / Rihards Veips
-// 
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>
 // ---------------------------------------------------------------------
 // [CFXS] //
 #include <CFXS/CNC/G_Man.hpp>
 #include <CFXS/Base/Debug.hpp>
-#include <ctype.h>
+
+#define IS_SPACE(x) (c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r' || c == ' ' ? 1 : 0)
 
 namespace CFXS::CNC {
 
@@ -58,7 +59,7 @@ namespace CFXS::CNC {
         while (readPos < len) {
             char c = cmd[readPos];
 
-            if (isspace(c)) {
+            if (IS_SPACE(c)) {
                 readPos++;
                 continue;
             } else {
